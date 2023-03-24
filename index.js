3d3fee474496192eeb8a01da14f8917aa7e6f965
Henish Patel <200120107182@git.org.in>
2023-03-24 15:42:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const httpProxy = require("http-proxy");
+
+const target = "http://3.6.211.9:5100"
+
+const proxy = httpProxy.createProxyServer({
+    target: target,
+})
+
+proxy.on("error", (err, req, res) => {
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+    res.end("Something went wrong, And we are reporting a custom error message.")
+})
+
+const port = 3000;
+
+proxy.listen(port, () => {
+    console.log(`Proxy server is running on port ${port}........`);
+});
