27d11deab7b876446600570a5641dfa2cd21b440
Henish Patel <200120107182@git.org.in>
2023-04-12 09:56:42 +0530

    ..

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const http = require('http');
-const httpProxy = require('http-proxy');
+const http = require("http");
+const httpProxy = require("http-proxy");
 
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
-  const proxyUrl = req.headers['x-proxy-url'];
+  const proxyUrl = req.headers["x-proxy-url"];
 
-  if (proxyUrl) {
-    proxy.web(req, res, { target: proxyUrl });
-  } else {
-    res.statusCode = 400;
-    res.end('No X-Proxy-Url header specified');
+  if (!proxyUrl) {
+    proxyUrl = "http://3.6.211.9:5100";
   }
+  proxy.web(req, res, { target: proxyUrl });
 });
 
 server.listen(8080, () => {
-  console.log('Proxy server listening on port 8080');
+  console.log("Proxy server listening on port 8080");
 });
